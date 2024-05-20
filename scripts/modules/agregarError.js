dc471cabc90f66524a24b8ca9ea203fa4e653187
Emily <nemily1710@gmail.com>
2024-05-20 12:16:06 -0500

    feat: :sparkles: Se agrega alerta de errores

--- scripts/modules/agregarError.js
+++ scripts/modules/agregarError.js
@@ -1,18 +1,18 @@
-import Variables from "../variables.js";
-
+import Variables from '../variables.js';
 
 const variables = new Variables();
 export default function agregarError(error) {
     //Verificar que el error renga un mensaje
     if (!error) {
-        console.error("El error debe tener un mensaje");
+        console.error('El error debe tener un mensaje');
         return;
     }
     //Aca clonamos el nodo desde variables para manejarlo e insertarlo en la lista de errores
     const errorMsgTemplate = variables.errorMessageEx.cloneNode(true);
-    errorMsgTemplate.querySelector(".errorMessage").innerHTML = error;
-    errorMsgTemplate.style.opacity = "1";
-    variables.errorMessagesList.insertAdjacentElement("beforeend", errorMsgTemplate);
+    errorMsgTemplate.querySelector('.errorMessage').innerHTML = error;
+    errorMsgTemplate.style.opacity = '1';
+    variables.errorMessagesList.style.display = 'flex';
+    variables.errorMessagesList.insertAdjacentElement('beforeend', errorMsgTemplate);
 
     //Extraemos el ultimo error
     const lastErrorDiv = variables.errorMessagesList.lastElementChild;
@@ -21,14 +21,15 @@ export default function agregarError(error) {
     const transparentar = () => {
         let lastOpacity = parseFloat(lastErrorDiv.style.opacity);
         if (lastOpacity > 0) {
-            lastErrorDiv.style.opacity = (lastOpacity - 0.05).toFixed(2); 
+            lastErrorDiv.style.opacity = (lastOpacity - 0.05).toFixed(2);
         } else {
             //Limpiar el intervalo cuando es transparente y eliminarmos el div del error de la lista
-            clearInterval(newInterval); 
-            variables.errorMessagesList.removeChild(lastErrorDiv); 
+            clearInterval(newInterval);
+            variables.errorMessagesList.removeChild(lastErrorDiv);
+            variables.errorMessagesList.style.display = 'none';
         }
     };
 
     //Creamos el intervalo que llama a la funcion transparentar sobre el div
-    const newInterval = setInterval(transparentar, 250); 
+    const newInterval = setInterval(transparentar, 250);
 }
